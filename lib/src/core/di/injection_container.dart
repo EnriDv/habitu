66057ffcb85080487fd5d99a99ea1e277aee2792
Enrique Diaz <diazenrive@gmail.com>
2026-06-11 13:16:58 -0400

    feat(core):inyector de dependencias

--- lib/src/core/di/injection_container.dart
+++ lib/src/core/di/injection_container.dart
@@ -1,26 +1,37 @@
 import 'package:get_it/get_it.dart';
 import '../network/custom_http_client.dart';
+import '../database/app_database.dart';
 import '../../features/habits/data/datasources/habits_local_datasource.dart';
 import '../../features/habits/data/datasources/habits_remote_datasource.dart';
 import '../../features/habits/data/repositories/habits_repository_impl.dart';
 import '../../features/habits/domain/repositories/habits_repository.dart';
 import '../../features/habits/presentation/notifiers/habits_notifier.dart';
-
+import '../../features/onboarding/presentation/notifiers/onboarding_notifier.dart';
 
 final sl = GetIt.instance;
 
-
 Future<void> init() async {
+  // Database
+  sl.registerLazySingleton<AppDatabase>(() => AppDatabase());
+
+  // Network
   sl.registerLazySingleton<CustomHttpClient>(() => CustomHttpClient());
 
+  // Onboarding Feature
+  sl.registerLazySingleton<OnboardingNotifier>(
+    () => OnboardingNotifier(db: sl<AppDatabase>()),
+  );
+
+  // Habits Feature DataSources
   sl.registerLazySingleton<HabitsRemoteDataSource>(
     () => HabitsRemoteDataSource(),
   );
 
   sl.registerLazySingleton<HabitsLocalDataSource>(
-    () => HabitsLocalDataSource(),
+    () => HabitsLocalDataSource(db: sl<AppDatabase>()),
   );
 
+  // Habits Feature Repository
   sl.registerLazySingleton<HabitsRepository>(
     () => HabitsRepositoryImpl(
       localDataSource: sl<HabitsLocalDataSource>(),
@@ -28,6 +39,7 @@ Future<void> init() async {
     ),
   );
 
+  // Habits Feature Notifiers
   sl.registerLazySingleton<HabitsNotifier>(
     () => HabitsNotifier(repository: sl<HabitsRepository>()),
   );
